6f672af84b7560f0b3c365adb52cdcde73e24ba9
Himanshu Joshi <Himanshu_Joshi@external.mckinsey.com>
2015-08-06 19:22:15 +0530

    db design update

--- db/migrate/20150728110922_create_topics.rb
+++ db/migrate/20150728110922_create_topics.rb
@@ -1,7 +1,7 @@
 class CreateTopics < ActiveRecord::Migration
   def change
     create_table :topics do |t|
-    	t.integer :portfolio_id, null: false
+    	t.integer :category_id, null: false
     	t.string :title, null: false
     	t.string :description, null: false
     	t.integer :owner_id, null: false
